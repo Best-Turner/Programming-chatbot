a2857ed5f5b0f7fe47958042dd6f5f54e48a750a
Aleksandr Trofimov <shurikx6881@gmail.com>
2024-12-18 22:30:38 +0300

    feat: попробовать работу аспекта

--- src/main/java/org/example/aspect/LoggingAspect.java
+++ src/main/java/org/example/aspect/LoggingAspect.java
@@ -1,4 +1,23 @@
 package org.example.aspect;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
 public class LoggingAspect {
+
+    private Logger logger = LogManager.getLogger(LoggingAspect.class);
+
+    @Around("execution(* org.example.service.impl.*.*(..))")
+    public String log(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        logger.info("Начало выполнения метода");
+        String result = (String) proceedingJoinPoint.proceed();
+        logger.info("Конец выполнения метода");
+        return result;
+    }
 }
--- src/main/java/org/example/service/impl/ChatBotService.java
+++ src/main/java/org/example/service/impl/ChatBotService.java
@@ -1,5 +1,8 @@
 package org.example.service.impl;
 
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.example.repository.KeywordStorage;
 import org.example.service.LessonService;
 import org.example.service.strategy.ResponseStrategy;
